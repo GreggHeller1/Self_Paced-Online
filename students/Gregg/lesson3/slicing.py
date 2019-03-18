3b7846d10f65bd5721ecceef3b68ac821a2e3e0e
Greggory Heller <Gregg@Greggorys-MacBook-Pro-506.local>
2019-03-17 18:17:47 -0700

    Finished slicing tasks

--- students/Gregg/lesson3/slicing.py
+++ students/Gregg/lesson3/slicing.py
@@ -8,6 +8,7 @@ import copy
 
 a_string = "this is a string"
 a_tuple = (2, 54, 13, 12, 5, 32)
+a_long_tuple = (2, 54, 13, 12, 5, 32)*2
 
 def exchange_last_first(sequence):
     """Reorder sequence switching the first and last elements"""
@@ -22,7 +23,21 @@ def skip_every_other(sequence):
     new_sequence = copy.copy(sequence)
     return new_sequence[::2]
 
+def center_skip_every_other(sequence):
+    """Remove first 4 and the last 4 items removed, and then every other item"""
+    new_sequence = copy.copy(sequence)
+    return new_sequence[4:-4][::2]
 
+def reverse(sequence):
+    """Reverse the sequence"""
+    new_sequence = copy.copy(sequence)
+    return new_sequence[::-1]
+
+def mid_last_first(sequence):
+    """Move the first third of the sequence to the end"""
+    new_sequence = copy.copy(sequence)
+    third_length = round(len(sequence)/3)
+    return new_sequence[third_length:]+new_sequence[:third_length]
 
 def tests():
     """Test the ________ functions"""
@@ -30,17 +45,24 @@ def tests():
     assert exchange_last_first(a_tuple) == (32, 54, 13, 12, 5, 2)
     assert skip_every_other(a_string) == "ti sasrn"
     assert skip_every_other(a_tuple) == (2, 13, 5)
-    #assert mid_last_first(a_string) == "is a stringthis "
-    #assert mid_last_first(a_tuple) == (13, 12, 5, 32, 2, 54)
+    assert center_skip_every_other(a_string) == " sas"
+    assert center_skip_every_other(a_long_tuple) == (5, 2)
+    assert reverse(a_string) == "gnirts a si siht"
+    assert reverse(a_tuple) == (32, 5, 12, 13, 54, 2)
+    assert mid_last_first(a_string) == "is a stringthis "
+    assert mid_last_first(a_tuple) == (13, 12, 5, 32, 2, 54)
+
+def exercise_demo(func_in, string_in = a_string, tuple_in = a_tuple):
+    print(f'Lesson3: Slicing Exercise - {func_in.__doc__}')
+    print(f'{string_in} becomes {func_in(string_in)}')
+    print(f'{tuple_in} becomes {func_in(tuple_in)}')
+    print('')
 
 if __name__ == "__main__":
     tests()
     print('Lesson3: Slicing Exercise')
-    print(f'LessonX: _______ Exercise - {exchange_last_first.__doc__}')
-    print(f'{a_string} becomes {exchange_last_first(a_string)}')
-    print(f'{a_tuple} becomes {exchange_last_first(a_tuple)}')
-    print('')
-    print(f'LessonX: _______ Exercise - {skip_every_other.__doc__}')
-    print(f'{a_string} becomes {skip_every_other(a_string)}')
-    print(f'{a_tuple} becomes {skip_every_other(a_tuple)}')
-    print('')
+    exercise_demo(exchange_last_first)
+    exercise_demo(skip_every_other)
+    exercise_demo(center_skip_every_other, tuple_in = a_long_tuple)
+    exercise_demo(reverse)
+    exercise_demo(mid_last_first)
